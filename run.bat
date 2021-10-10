58397ea1d75129cb5cadbefde32a3ce6d932d106
Kyium <kiu4537@yahoo.co.uk>
2021-10-11 00:57:02 +0100

    updated run.bat
    now will skip venv creation attempt if directory exists

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-python -m venv win_venv
-.\win_venv\Scripts\activate
+@echo off
+if not exist ./win_venv/ python -m venv win_venv
 .\win_venv\Scripts\pip install -r requirements.txt
 .\win_venv\Scripts\python main.py
